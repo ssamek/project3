460eb5a9b8bebcade910de7da6cc76d0df1e3de9
ssamek <123518145+ssamek@users.noreply.github.com>
2023-04-18 23:18:59 -0500

    adding text box

--- js/script.js
+++ js/script.js
@@ -19,6 +19,13 @@ function init() {
     animation: google.maps.Animation.BOUNCE,
 	icon: 'images/flag.png'
   });
+  
+ var contentString = '<h1>Muiderslot</h1><p>Muiden Castle is currently a national museum. The inside of the castle, its rooms and kitchens, was restored to look like they did in the 17th century and several of the rooms now house a collection of arms and armour.</p>';
+  
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString,
+	maxWidth: 250,
+  });
   google.maps.event.addListener(marker, 'mouseover', function() {
     infowindow.open(myMap, marker);
   });
