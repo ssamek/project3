649e8fa7b3005c44dfee8afcc7bcc734d758aff4
ssamek <123518145+ssamek@users.noreply.github.com>
2023-04-18 22:55:21 -0500

    adding javascript

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function init() {
+  //alert('it works');
+  var el = document.getElementById('canvas');
+  var myLocation = new google.maps.LatLng(-36.8593557,174.3764728);
+  var mapOptions = {
+    center: myLocation,
+	zoom: 12,
+	mapTypeId: google.maps.MapTypeId.SATELLITE,
+	mapTypeControlOptions: {
+      position: google.maps.ControlPosition.BOTTOM_CENTER
+    }
+  };
