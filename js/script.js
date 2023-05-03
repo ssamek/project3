acb3cff52a3684431465cc42deec6852cd70b8b0
ssamek <123518145+ssamek@users.noreply.github.com>
2023-05-02 22:04:57 -0500

    experiment 3

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let map;
+/*let map;
 function init() {
   //alert('it works');
   var el = document.getElementById('canvas');
@@ -33,4 +33,32 @@ google.maps.event.addListener(marker, 'mouseover', function() {
   });
   */
 //google.maps.event.addDomListener(window, 'load', init);
-init();
+//init();
+
+// Initialize and add the map
+let map;
+
+async function initMap() {
+  // The location of Uluru
+  const position = { lat: -25.344, lng: 131.031 };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
+
+  // The map, centered at Uluru
+  map = new Map(document.getElementById("canvas"), {
+    zoom: 4,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerView({
+    map: map,
+    position: position,
+    title: "Uluru",
+  });
+}
+
+initMap();
